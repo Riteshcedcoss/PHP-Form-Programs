3e9f186b37138316b598ee21a504ee473487a358
Riteshcedcoss <riteshkumargupta@cedcoss.com>
2022-02-15 14:10:24 +0530

    Second

--- src/index.php
+++ src/index.php
@@ -1,58 +1,51 @@
+<?php
+$first_num = $_POST['first_num'];
+$second_num = $_POST['second_num'];
+$operator = $_POST['operator'];
+$result = '';
+if (is_numeric($first_num) && is_numeric($second_num)) {
+    switch ($operator) {
+        case "Add":
+           $result = $first_num + $second_num;
+            break;
+        case "Subtract":
+           $result = $first_num - $second_num;
+            break;
+        case "Multiply":
+            $result = $first_num * $second_num;
+            break;
+        case "Divide":
+            $result = $first_num / $second_num;
+    }
+}
+
+?>
+
 <!DOCTYPE html>
 
 <head>
-    <title> Calculate Electricity Bill</title>
+    <title> Calculator Program</title>
 </head>
 
-<?php
-
-$result_str = $result = '';
-if (isset($_POST['unit-submit'])) {
-    $units = $_POST['units'];
-    if (!empty($units)) {
-        $result = calculate_bill($units);
-        $result_str = 'Total amount of ' . $units . ' - ' . $result;
-    }
-}
-
-function calculate_bill($units)
-{
-    $firstUnit = 3.50;
-    $secondUnit = 4.00;
-    $thirdUnit = 5.20;
-    $fourthUnit = 6.50;
-
-    if ($units <= 50) {
-        $bill = $units * $firstUnit;
-    } else if ($units > 50 && $units <= 100) {
-        $temp = 50 * $firstUnit;
-        $remaining_units = $units - 50;
-        $bill = $temp + ($remaining_units * $secondUnit);
-    } else if ($units > 100 && $units <= 200) {
-        $temp = (50 * 3.5) + (100 * $secondUnit);
-        $remaining_units = $units - 150;
-        $bill = $temp + ($remaining_units * $thirdUnit);
-    } else {
-        $temp = (50 * 3.5) + (100 * $secondUnit) + (100 * $thirdUnit);
-        $remaining_units = $units - 250;
-        $bill = $temp + ($remaining_units * $fourthUnit);
-    }
-    return number_format((float)$bill, 2, '.', '');
-}
-?>
 
 <body>
-    <div id="page-wrap">
-        <h1>Calculate Electricity Bill</h1>
-
-        <form action="" method="post" id="quiz-form">
-            <input type="number" name="units" id="units" placeholder="Please enter no. of Units" />
-            <input type="submit" name="unit-submit" id="unit-submit" value="Submit" />
-        </form>
-
-        <div>
-            <?php echo '<br />' . $result_str; ?>
-        </div>
+<div id="page-wrap">
+	<h1> Calculator Program</h1>
+	  <form action="" method="post" id="quiz-form">
+            <p>
+                <input type="number" name="first_num" id="first_num" required="required" value="<?php echo $first_num; ?>" /> <b>Number 1</b>
+            </p>
+            <p>
+                <input type="number" name="second_num" id="second_num" required="required" value="<?php echo $second_num; ?>" /> <b>Number 2</b>
+            </p>
+            <p>
+                <input readonly="readonly" name="result" value="<?php echo $result; ?>"> <b>Result</b>
+            </p>
+            <input type="submit" name="operator" value="Add" />
+            <input type="submit" name="operator" value="Subtract" />
+            <input type="submit" name="operator" value="Multiply" />
+            <input type="submit" name="operator" value="Divide" />
+	  </form>
     </div>
 </body>
 
