430b28445e6f69bd49fe4ebe791843246996272b
Riteshcedcoss <riteshkumargupta@cedcoss.com>
2022-02-15 16:45:47 +0530

    Fourth

--- src/index.php
+++ src/index.php
@@ -1,44 +1,53 @@
 <?php
-$area=0;
-$perimeter=0;
-$len="";
-$wid="";
-if(isset($_POST["calcBtn"]))
-{
-    $len= $_POST["len"];
-    $wid= $_POST["wid"];
-    
-    //area 
-    $area=$len*$wid;
- 
-    //perimeter 
-    $perimeter=2*($len+$wid);
-}
- 
+$input=$_POST['input'];
+$changeTime= $_POST['time'];
+$result;
 
+if(isset($_POST['convert'])){
+print_r($_POST);
+    if($changeTime=="hours_to_min"){
+$result=60*$input." min";
+}
+
+elseif($changeTime=="hours_to_sec"){
+$result= (60*60)*$input." seconds";
+}
+
+}
 ?>
 
+
 <!DOCTYPE html>
-
+<html lang="en">
 <head>
-    <title> Cal. Area And Peri.</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
-
-
 <body>
-<div id="page-wrap">
-        <form action="" method="post">
-            <label for="len">Length:
-                <input type="text" name="len" value="<?=$len?>"><br><br>
-            </label>
-            <label for="wid">Width:
-                <input type="text" name="wid" value="<?=$wid?>"><br><br>
-            </label>
-            <button name="calcBtn" type="submit">Calculate Area and Perimeter</button>
-        </form> 
-        <h2>Area: <?=$area?></h2>
-        <h2>Perimeter:<?=$perimeter?></h2>
-    </div>
-</body>
+<form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
 
+
+<input type="text" name="input"><br><br>
+
+<input type="radio" id="" name="time" value="hours_to_min">
+<label for="">hours to min</label><br>
+
+<input type="radio" id="" name="time" value="hours_to_sec">
+<label for="">hours to sec</label><br>
+
+<input type="submit" name="convert" value="convert">
+
+
+<p>
+    <?php
+    echo $result;
+    ?>
+</p>
+
+</form>
+
+  
+</body>
 </html>
